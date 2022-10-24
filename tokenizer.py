9f5292fa01ac625385141c969dbcef2a517c758c
matthew-burnard <mburnard@gmu.edu>
2022-10-24 15:39:16 -0400

    Added non-functional model file

--- tokenizer.py
+++ tokenizer.py
@@ -1,9 +1,19 @@
+import numpy as np
+
+class LanguageNotImplementedException(Exception):
+  pass
+class CharNotInAlphabetException(Exception):
+  pass
+class InputMismatchException(Exception):
+  pass
+
+
 class Tokenizer:
   def __init__(self, lang='en',
       pad_idx=0, pad_tok='[PAD]', start_idx=1, start_tok='[START]', end_idx=2, end_tok='[END]', space_idx=3, space_tok='[SPACE]'):
     #set alphabet used by language
     if lang=='en':
-      self.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+      alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     else:
       raise LanguageNotImplementedException(f"Language not implemented: {lang}")
     #Set our special tokens
@@ -16,17 +26,18 @@ class Tokenizer:
     self.space_idx=space_idx
     self.space_tok=space_tok
     #Add tokens to the alphabet
-    self.alphabet.insert(pad_idx, self.pad_tok)
-    self.alphabet.insert(start_idx, self.start_tok)
-    self.alphabet.insert(end_idx, self.end_tok)
-    self.alphabet.insert(space_idx, self.space_tok)
+    alphabet.insert(pad_idx, self.pad_tok)
+    alphabet.insert(start_idx, self.start_tok)
+    alphabet.insert(end_idx, self.end_tok)
+    alphabet.insert(space_idx, self.space_tok)
+    self.alphabet=alphabet
     self.alphabet_size=len(self.alphabet)
     
-    print(f"Alphabet: self.alphabet")
-    print(f"Alphabet size: self.alphabet_size")
+    print(f"Alphabet: {self.alphabet}")
+    print(f"Alphabet size: {self.alphabet_size}")
   
   ## Returns a list of token indexes for each character in the input string, padded to max_len
-  def tokenize(self, string_in, max_len, pad=True):
+  def tokenize(self, string_in, max_len=-1, pad=True):
     list_out=[]
     list_out.append(self.start_idx)
     for char in string_in:
@@ -41,9 +52,11 @@ class Tokenizer:
       list_out.append(char_idx)
     list_out.append(self.end_idx)
     if pad==True:
+      if max_len==-1:
+        raise InputMismatchException("Must specify max length to pad")
       for i in range(len(list_out), max_len):
         list_out.append(self.pad_idx)
-    return list_out
+    return np.array(list_out)
   
   ## Returns a list of the tokens from a token list
   def print_tok_list(self, list_in):
