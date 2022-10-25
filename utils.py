0e7a985e3bc94cecbc012346a430893c84a0119f
matthew-burnard <mburnard@gmu.edu>
2022-10-25 00:44:19 -0400

    Updated utils.make_data_splits

--- utils.py
+++ utils.py
@@ -1,19 +1,22 @@
 from random import shuffle
 
-def make_data_splits(filepath, ratio=0.8):
-  with open(filepath, 'r') as file:
-    lines = file.readlines()
-  train_size=ratio
-  train_idx_end=len(lines)*train_size
-  dev_size=(1-ratio)/2
-  dev_idx_end=train_idx_end+len(lines)*dev_size
-  test_size=(1-ratio)/2
-  
-  shuffle(lines)
-  
-  train_lines = lines[:train_idx_end]
-  dev_lines=lines[train_idx_end:dev_idx_end]
-  test_lines=lines[dev_idx_end:]
+def make_data_splits(filepath_list, ratio=0.8):
+  train_lines,dev_lines,test_lines=[],[],[]
+  for filepath in filepath_list:
+    with open(filepath, 'r') as file:
+      lines = lines + file.readlines()
+    
+    train_size=ratio
+    train_idx_end=len(lines)*train_size
+    dev_size=(1-ratio)/2
+    dev_idx_end=train_idx_end+len(lines)*dev_size
+    test_size=(1-ratio)/2
+    
+    shuffle(lines)
+    
+    train_lines = train_lines + lines[:train_idx_end]
+    dev_lines = dev_lines + lines[train_idx_end:dev_idx_end]
+    test_lines = test_lines + lines[dev_idx_end:]
   
   train_file=open("data.train", 'w')
   for line in trainlines:
