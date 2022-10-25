179f8dc13994930deba13f624c1a6820f89e1761
matthew-burnard <mburnard@gmu.edu>
2022-10-25 00:38:15 -0400

    Updated tokenizer function

--- tokenizer.py
+++ tokenizer.py
@@ -1,9 +1,12 @@
+import alphabets
+
 class Tokenizer:
   def __init__(self, lang='en',
-      pad_idx=0, pad_tok='[PAD]', start_idx=1, start_tok='[START]', end_idx=2, end_tok='[END]', space_idx=3, space_tok='[SPACE]'):
+      pad_idx=0, pad_tok='[PAD]', start_idx=1, start_tok='[SOS]', end_idx=2, end_tok='[EOS]', space_idx=3, space_tok='[SPACE]', lang_idx=4):
     #set alphabet used by language
-    if lang=='en':
-      self.alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    if alphabet=='latin':
+      self.alphabet = alphabets.LATIN_SIMPLE_ALPHABET
+      lang_tok='[ENG]'
     else:
       raise LanguageNotImplementedException(f"Language not implemented: {lang}")
     #Set our special tokens
@@ -15,11 +18,14 @@ class Tokenizer:
     self.end_tok=end_tok
     self.space_idx=space_idx
     self.space_tok=space_tok
+    self.lang_idx=lang_idx
+    self.lang_tok=lang_tok
     #Add tokens to the alphabet
-    self.alphabet.insert(pad_idx, self.pad_tok)
-    self.alphabet.insert(start_idx, self.start_tok)
-    self.alphabet.insert(end_idx, self.end_tok)
-    self.alphabet.insert(space_idx, self.space_tok)
+    self.alphabet.insert(self.pad_idx, self.pad_tok)
+    self.alphabet.insert(self.start_idx, self.start_tok)
+    self.alphabet.insert(self.end_idx, self.end_tok)
+    self.alphabet.insert(self.space_idx, self.space_tok)
+    self.alphabet.insert(self.lang_idx, self.lang_tok)
     self.alphabet_size=len(self.alphabet)
     
     print(f"Alphabet: {self.alphabet}")
@@ -28,6 +34,7 @@ class Tokenizer:
   ## Returns a list of token indexes for each character in the input string, padded to max_len
   def tokenize(self, string_in, max_len, pad=True):
     list_out=[]
+    list_out.append(self.lang_idx)
     list_out.append(self.start_idx)
     for char in string_in:
       char_idx=None
@@ -46,7 +53,7 @@ class Tokenizer:
     return list_out
   
   ## Returns a list of the tokens from a token list
-  def print_tok_list(self, list_in):
+  def str_of_tok_list(self, list_in):
     list_out = []
     for tok in list_in:
       list_out.append(self.alphabet[tok])
