4ac2cff77f1db478a5998c679d34501373a7ec97
matthew-burnard <mburnard@gmu.edu>
2022-10-27 15:16:28 -0400

    Updated builddata.py

--- builddata.py
+++ builddata.py
@@ -2,6 +2,30 @@ from joeynmt.vocabulary import Vocabulary
 from pathlib import Path
 from alphabets import LATIN_EXTENDED_ALPHABET
 import utils
+from tokenizer import Tokenizer
+
+
+def raw_to_src():
+  with open("langs/english_example.txt", 'r') as file:
+    en_lines = file.readlines()
+
+  batch_size=5
+
+  targets,inputs=[],[]
+  for line in en_lines[:batch_size]:
+    target,input = line.lower().strip().split(',')
+    inputs.append(input)
+    targets.append(target)
+
+  max_len=max([len(str) for str in inputs])+2
+
+  tokenizer = Tokenizer(lang='en')
+
+  target_toks,input_toks = [],[]
+  for target_str,input_str in zip(targets,inputs):
+    target_tok =tokenizer.tokenize(target_str, max_len)
+    target_toks.append(target_tok)
+
 
 #Make vocabulary
 def build_and_save_vocab():
@@ -16,4 +40,5 @@ def make_test_datasets():
   utils.make_data_splits(filepath_list, langs_list)
 
 if __name__=="__main__":
+  raw_to_src()
   make_test_datasets()
